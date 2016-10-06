ae211d61f65c75aaabcb92f33d3848265fbe9c2c
ZhilkoRoman <roman.zhilko@gmail.com>
2016-10-06 21:51:19 +0300

    first commit

---
+++ app/src/main/java/com/example/notepadby/russiancourse/LevelActivity.java
@@ -0,0 +1,141 @@
+package com.example.notepadby.russiancourse;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Spannable;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.w3c.dom.Text;
+
+import java.text.BreakIterator;
+import java.util.Locale;
+
+public class LevelActivity extends Activity {
+
+    boolean taskFinished = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level);
+        ((TextView) findViewById(R.id.taskNumber)).setText("Задание 1");
+        ((TextView) findViewById(R.id.taskView)).setText("Выделите в тексте слова с ошибками");
+        ((TextView) findViewById(R.id.taskText)).setText("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+        init();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0, 1, 0, "Назад");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (taskFinished) {
+            finish();
+        } else {
+            createDialog();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    DialogInterface.OnClickListener alertListener = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            switch (which){
+                case AlertDialog.BUTTON_POSITIVE:
+                    break;
+                case AlertDialog.BUTTON_NEGATIVE:
+                    finish();
+                    break;
+            }
+        }
+    };
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK)) {
+            if (taskFinished) {
+                finish();
+            } else {
+                createDialog();
+            }
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+
+    JSONArray getTasks(){
+        //method to download tasks from server
+        JSONArray tasks = new JSONArray();
+        //getting from server
+        return tasks;
+    }
+
+    void createDialog(){
+        AlertDialog.Builder dialog = new AlertDialog.Builder(LevelActivity.this);
+        dialog.setTitle("Внимание");
+        dialog.setMessage("Вы не завершили задание. Если вы выйдете, прогресс не сохранится. Вы уверены, что хотите выйти?");
+        dialog.setPositiveButton("Нет", alertListener);
+        dialog.setNegativeButton("Да", alertListener);
+        dialog.show();
+    }
+
+    private void init() {
+        String definition = ((TextView) findViewById(R.id.taskText)).getText().toString();
+        TextView definitionView = (TextView) findViewById(R.id.taskText);
+        definitionView.setMovementMethod(LinkMovementMethod.getInstance());
+        definitionView.setText(definitionView.getText(), TextView.BufferType.SPANNABLE);
+        Spannable spans = (Spannable) definitionView.getText();
+        BreakIterator iterator = BreakIterator.getWordInstance(Locale.US);
+        iterator.setText(definition);
+
+        int start = iterator.first();
+        for (int end = iterator.next(); end != BreakIterator.DONE; start = end, end = iterator
+                .next()) {
+            String possibleWord = definition.substring(start, end);
+            if (Character.isLetterOrDigit(possibleWord.charAt(0))) {
+                ClickableSpan clickSpan = getClickableSpan(possibleWord);
+                TextPaint ds = new TextPaint();
+                ds.setUnderlineText(false);
+                clickSpan.updateDrawState(ds);
+                spans.setSpan(clickSpan, start, end,
+                        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            }
+        }
+    }
+
+    private ClickableSpan getClickableSpan(final String word) {
+        return new ClickableSpan() {
+            final String mWord;
+            {
+                mWord = word;
+            }
+
+            @Override
+            public void onClick(View widget) {
+                Toast.makeText(LevelActivity.this,"tapped on:"+ mWord, Toast.LENGTH_LONG).show();
+            }
+
+            public void updateDrawState(TextPaint ds) {
+                super.updateDrawState(ds);
+            }
+        };
+    }
+}
---
+++ app/src/main/java/com/example/notepadby/russiancourse/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.notepadby.russiancourse;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class MainActivity extends Activity {
+
+    EditText login;
+    EditText password;
+    Button submit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        login = (EditText) findViewById(R.id.login);
+        password = (EditText) findViewById(R.id.password);
+        submit = (Button) findViewById(R.id.submitLogin);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (login.getText().length() == 0) {
+                    Toast.makeText(MainActivity.this, "Введите логин", Toast.LENGTH_LONG).show();
+                } else if (password.getText().length() == 0) {
+                    Toast.makeText(MainActivity.this, "Введите пароль", Toast.LENGTH_LONG).show();
+                } else {
+                    Intent intent = new Intent(MainActivity.this, LevelActivity.class);
+                    startActivity(intent);
+                }
+            }
+
+        });
+    }
+}
